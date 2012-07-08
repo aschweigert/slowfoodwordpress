f3df09d9cb7b5f8afc91ce5b576487d1954c8534
Adam Schweigert <aschweigert@gmail.com>
2012-07-08 17:44:28 -0400

    a less mess way to get the social media links

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
-     
+
     require_once( TEMPLATEPATH . '/theme-settings.php' );
-     	
+
 	// Add RSS links to <head> section
 	automatic_feed_links();
 
@@ -12,8 +12,7 @@
     	remove_action('wp_head', 'wp_generator' );
     }
     add_action('init', 'removeHeadLinks');
-    remove_action('wp_head', 'wp_generator');
-    
+
     if (function_exists('register_sidebar')) {
     	register_sidebar(array(
     		'name' => __('Sidebar Widgets','slowfood' ),
@@ -25,10 +24,10 @@
     		'after_title'   => '</h2>'
     	));
     }
-    
+
     // Add a custom post types for the homepage carousel + event listings
     add_action( 'init', 'create_post_type' );
-    
+
     function create_post_type() {
 		register_post_type( 'sf_homepage-banner',
 			array(
@@ -40,7 +39,7 @@
 					'has_archive' => true,
 				)
 			);
-			
+
 		register_post_type( 'sf_events',
 			array(
 				'labels' => array(
@@ -52,8 +51,86 @@
 				)
 			);
 	}
-	
-    
+
+	// add user contact methods
+    function bootstrap_contactmethods( $contactmethods ) {
+	    // Add Twitter
+	    if ( !isset( $contactmethods['twitter'] ) )
+	    $contactmethods['twitter'] = 'Twitter';
+
+	    // Add Facebook
+	    if ( !isset( $contactmethods['facebook'] ) )
+	    $contactmethods['Facebook'] = 'Facebook';
+
+	    // Add Google+
+	    if ( !isset( $contactmethods['gplus'] ) )
+	    $contactmethods['gplus'] = 'Google+';
+
+	    // Remove Yahoo IM
+	    if ( isset( $contactmethods['yim'] ) )
+	    unset( $contactmethods['yim'] );
+
+	    // Remove AIM
+	    if ( isset( $contactmethods['aim'] ) )
+	    unset( $contactmethods['aim'] );
+
+	    return $contactmethods;
+	}
+
+	add_filter( 'user_contactmethods', 'bootstrap_contactmethods', 10, 1 );
+
+	// a little function to retrieve to the social media links
+	function slowfood_social_links () {
+
+		$fields = array(
+			'rss' => 'Link to RSS Feed',
+			'facebook' => 'Link to Facebook Profile',
+			'twitter' => 'Link to Twitter Page',
+			'youtube' => 'Link to YouTube Page',
+			'flickr' => 'Link to Flickr Page',
+			'gplus' => 'Link to Google Plus Page',
+			'pinterest' => 'Link to Pinterest Profile',
+			'linkedin' => 'Link to LinkedIn Profile or Group'
+		);
+
+		foreach ( $fields as $field => $title ) {
+			$field_link = $field . '_link';
+			$directory = get_bloginfo('template_directory');
+
+			if ( get_option( $field_link ) ) :
+				if ( $field == 'gplus' ) {
+					echo '<li><a href="' . esc_url( get_option( $field_link ) ) . '" title="' . $title . '" rel="me"><img src="' . get_bloginfo('template_directory') . '/assets/img/social-icons/' . $field . '.png" alt="' . $field .'-fav" /></a></li>';
+				} else {
+					echo '<li><a href="' . esc_url( get_option( $field_link ) ) . '" title="' . $title . '"><img src="' . get_bloginfo('template_directory') . '/assets/img/social-icons/' . $field . '.png" alt="' . $field .'-fav" /></a></li>';
+				}
+			endif;
+		}
+	}
+
+	// add thumbnail support, use the first image in a post if a user forget to set a featured image
+	add_theme_support( 'post-thumbnails' );
+
+	function autoset_featured_image() {
+          global $post;
+          $already_has_thumb = has_post_thumbnail($post->ID);
+              if (!$already_has_thumb)  {
+              $attached_image = get_children( "post_parent=$post->ID&post_type=attachment&post_mime_type=image&numberposts=1" );
+                          if ($attached_image) {
+                                foreach ($attached_image as $attachment_id => $attachment) {
+                                set_post_thumbnail($post->ID, $attachment_id);
+                                }
+                           }
+                        }
+      }
+
+	add_action('the_post', 'autoset_featured_image');
+	add_action('save_post', 'autoset_featured_image');
+	add_action('draft_to_publish', 'autoset_featured_image');
+	add_action('new_to_publish', 'autoset_featured_image');
+	add_action('pending_to_publish', 'autoset_featured_image');
+	add_action('future_to_publish', 'autoset_featured_image');
+
+
     // Hide the admin bar (for now)
     add_filter( 'show_admin_bar', '__return_false' );
 
--- navbar.php
+++ navbar.php
@@ -49,12 +49,12 @@
 			                        )); ?>
 
 			                        <?php while ($new_query->have_posts()) : $new_query->the_post(); update_post_caches($posts); ?>
-			                        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>		
+			                        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 			                        <?php endwhile; ?>
 		                    </ul>
 		                </li>
-		                
-		              </ul>	
+
+		              </ul>
 			      	</li>
 			      </ul>
 		          <div class="nav-collapse">
@@ -65,77 +65,13 @@
 			      		<li><a href="<?php bloginfo('url'); ?>/contact/">Contact</a></li>
 			      	</ul>
 		          	<ul class="social-icons pull-right">
-		          		
-		          		<?php if ( get_option( 'rss_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'rss_link' ) ); ?>" title="RSS">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/rss.png" alt="rss-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-						
-		          		<?php if ( get_option( 'facebook_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'facebook_link' ) ); ?>" title="Facebook">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/facebook.png" alt="facebook-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-			
-						<?php if ( get_option( 'twitter_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'twitter_link' ) ); ?>" title="twitter">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/twitter.png" alt="twitter-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-			
-						<?php if ( get_option( 'youtube_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'youtube_link' ) ); ?>" title="youtube">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/youtube.png" alt="youtube-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-			
-						<?php if ( get_option( 'flickr_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'flickr_link' ) ); ?>" title="flickr">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/flickr.png" alt="flickr-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-			
-						<?php if ( get_option( 'gplus_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'gplus_link' ) ); ?>" title="gplus">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/googleplus_black.png" alt="gplus-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-						
-						<?php if ( get_option( 'pinterest_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'pinterest_link' ) ); ?>" title="pinterest">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/pinterest.png" alt="pinterest-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-						
-						<?php if ( get_option( 'linkedin_link' ) ) : ?>
-							<li>
-								<a href="<?php echo esc_url( get_option( 'linkedin_link' ) ); ?>" title="linkedin">
-									<img src="<?php bloginfo('template_directory'); ?>/assets/img/social-icons/linkedin.png" alt="linkedin-fav" />
-								</a>
-							</li>
-						<?php endif; ?>
-
+		          		<?php slowfood_social_links(); ?>
 		          	</ul>
 			        <form method="get" name="searchform" id="searchform" action="<?php bloginfo('url'); ?>/" class="navbar-search form-inline pull-right">
 				        <input type="text" value="" name="s" id="query" placeholder="Search" />
 				    </form>
 		          </div>
-		          
+
 	          </div>
 	        </div>
 	    </nav>
